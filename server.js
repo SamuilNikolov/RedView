14a1c7c08b731f675daa530d71c3c094e23d143f
Samuil Nikolov <samuil_nikolov@abv.bg>
2025-09-28 22:06:32 -0400

    workday 9/26

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get('/curiosity', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'curiosity.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`RedView server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`RedView server running at http://0.0.0.0:${PORT}`);
 });
